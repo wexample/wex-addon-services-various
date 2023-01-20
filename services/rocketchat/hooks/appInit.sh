6a5d7c48f6aa440321fcd1e1c95f1667c9ae1a30
weeger <romain.weeger@gmail.com>
2023-01-20 14:33:53 +0100

    Command

--- services/rocketchat/hooks/appInit.sh
+++ services/rocketchat/hooks/appInit.sh
@@ -2,5 +2,5 @@
 
 rocketchatAppInit() {
   _wexLog "Initialize mongo DB replica set"
-  docker exec $(wex app/container -c=mongo) mongo --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"chat_mongo:27017\" } ]})"
+  docker exec $(wex app::app/container -c=mongo) mongo --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"chat_mongo:27017\" } ]})"
 }
