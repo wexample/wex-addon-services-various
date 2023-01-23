ea0d40f8e91d6b3165ae1ac374422387958902a4
weeger <romain.weeger@gmail.com>
2023-01-23 16:14:01 +0100

    Dumps list

---
+++ services/mongo/hooks/dbDumpsList.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mongoDbDumpsList() {
+  local FILES
+  local FILES_FILTERED
+  local DIR=.wex/mongo/dumps
+  FILES=$(find "${DIR}" -maxdepth  1 -exec basename {} \;)
+
+  # Initialize an empty array
+  FILES_TO_KEEP=()
+
+  # Loop through each file
+  for FILE in ${FILES[@]}; do
+      # Check if the file is a directory or ends with .zip
+      if [ -d "${DIR}/${FILE}" ] || [[ ${FILE} =~ \.zip$ ]]; then
+          FILES_TO_KEEP+=(${FILE})
+      fi
+  done
+
+  printf "%s\n" "${FILES_TO_KEEP[@]}"
+}
