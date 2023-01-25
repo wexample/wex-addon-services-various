66b47912202774bce494fdd6ff70768ba7fd03c6
weeger <romain.weeger@gmail.com>
2023-01-25 17:20:02 +0100

    Nextcloud install

--- services/nextcloud/hooks/appStarted.sh
+++ services/nextcloud/hooks/appStarted.sh
@@ -6,10 +6,27 @@ nextcloudAppStarted() {
   if [[ ${NEXTCLOUD_FIRST_START} == true ]]; then
     NEXTCLOUD_FIRST_START=false
 
-    _wexMessage "Restarting app including Nextcloud container"
+    _wexMessage "Nextcloud : Restarting app including container"
     wex app/restart
+
+    # Can't place this in appFirstStartInit, as this will be the second start of the app
+    # and APP_INITIALIZED should be set to true.
+    _wexLog "Nextcloud : Waiting for first install"
+    sleep 30
+
+    _wexLog "Nextcloud : Define configuration protocol"
+    local PROTOCOL=https;
+    if [[ ${APP_ENV} == local ]];then
+      PROTOCOL=http
+    fi
+
+    sudo wex file/textReplace -f=config.php -r="s/NEXTCLOUD_PROTOCOL/${PROTOCOL}/"
+
+    _wexLog "Nextcloud : Defining trusted hostname : ${DOMAIN_MAIN}"
+    wex app/exec -u=33 -c="php occ config:system:set trusted_domains 2 --value=${DOMAIN_MAIN}"
+
     return
   fi
 
-   _wexMessage "Nextcloud is installed and running"
+   _wexMessage "Nextcloud : Installed and running"
 }
--- services/nextcloud/samples/config.php
+++ services/nextcloud/samples/config.php
@@ -5,6 +5,6 @@ $CONFIG = [
     // Avoid a lot of file locking errors.
     'filelocking.enabled' => false,
     // Defined on app install.
-    'overwriteprotocol' => '${NEXTCLOUD_PROTOCOL}',
+    'overwriteprotocol' => 'NEXTCLOUD_PROTOCOL',
     'trashbin_retention_obligation' => 'auto, 1',
 ];
