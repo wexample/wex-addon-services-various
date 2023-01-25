19aea08cc41a0ec295c95d50a83e7b741d40aa2f
weeger <romain.weeger@gmail.com>
2023-01-25 16:02:20 +0100

    Add sample

---
+++ services/nextcloud/samples/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$CONFIG = [
+    'filesystem_check_changes' => 1,
+    // Avoid a lot of file locking errors.
+    'filelocking.enabled' => false,
+    // Defined on app install.
+    'overwriteprotocol' => '${NEXTCLOUD_PROTOCOL}',
+    'trashbin_retention_obligation' => 'auto, 1',
+];
