76a27f84cedd0d69d08159766ed62a385d18b4af
weeger <romain.weeger@gmail.com>
2023-02-01 09:08:31 +0100

    Various minor improvements

--- services/rocketchat/hooks/serviceInstall.sh
+++ services/rocketchat/hooks/serviceInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rocketchatAppInit() {
+rocketchatServiceInstall() {
   wex app::config/setValue -b -k=ROCKETCHAT_VERSION -v=5.4.0
   wex app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=rocketchat
 }
