366f5feaccb6558e560bd190cf6411204febb3e0
weeger <romain.weeger@gmail.com>
2023-02-05 16:28:49 +0100

    Jenkins

--- services/jenkins/hooks/appInit.sh
+++ services/jenkins/hooks/appInit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 jenkinsAppInit() {
-  wex app::config/setValue -b -k=JENKINS_VERSION -v=2.60.3
+  wex app::config/setValue -b -k=JENKINS_VERSION -v=2.389
   wex app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=jenkins
 }
