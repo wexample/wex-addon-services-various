e9259dc87fca60955bd2ff5fc395d30225f22cc0
weeger <romain.weeger@gmail.com>
2023-02-20 22:35:04 +0100

    Dead code

--- services/collabora/hooks/appInit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-collaboraAppInit() {
-  wex-exec app::config/setValue -b -k=COLLABORA_VERSION -v=22.05.9.2.1
-  wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=collabora
-}
