4ce469e1590dc5f54fbe269458199e8a3ad62b5a
weeger <romain.weeger@gmail.com>
2023-02-25 08:25:05 +0100

    Onlyoffice / Rocket Chat install

--- services/onlyoffice/hooks/serviceInstall.sh
+++ services/onlyoffice/hooks/serviceInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-onlyofficeAppInit() {
+onlyofficeServiceInstall() {
   wex-exec app::config/setValue -b -k=ONLYOFFICE_DOCUMENT_SERVER_VERSION -v=7.2.2
   wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=onlyoffice
 }
--- services/rocketchat/hooks/appFirstStartInit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rocketchatAppFirstStartInit() {
-  _wexLog "Initialize mongo DB replica set"
-
-  . "${WEX_FILEPATH_REL_CONFIG_BUILD}"
-  docker exec $(wex-exec app::app/container -c=mongo) mongosh --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"${APP_NAME}_mongo_6:27017\" } ]})"
-}
