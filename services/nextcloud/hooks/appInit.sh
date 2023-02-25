d9776954e9779a1472056217b990caa65001e601
weeger <romain.weeger@gmail.com>
2023-02-25 08:04:44 +0100

    Nextcloud install

--- services/nextcloud/hooks/appInit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-nextcloudAppInit() {
-  wex-exec app::config/setValue -b -k=NEXTCLOUD_VERSION -v=25.0.3
-  wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=nextcloud
-}
--- services/nextcloud/hooks/serviceInstall.sh
+++ services/nextcloud/hooks/serviceInstall.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 nextcloudServiceInstall() {
+  wex-exec app::config/setValue -b -k=NEXTCLOUD_VERSION -v=25.0.3
+  wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=nextcloud
+
   if [ "$(wex-exec app::service/user -s=mysql)" = "true" ] || [ "$(wex-exec app::service/user -s="mysql-8")" = "true" ] || [ "$(wex-exec app::service/user -s="maria-10")" = "true" ];then
     _wexLog "Adding mysql environment variables to php docker compose"
     local SERVICE_DIR
