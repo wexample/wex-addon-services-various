b69e33e61cff2e0f0a7e4eecfeb5fbf119b773be
weeger <romain.weeger@gmail.com>
2023-03-01 17:31:10 +0100

    Add grafana

---
+++ services/grafana/hooks/serviceInstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+grafanabServiceInstall() {
+  # Should be done later
+  # > set external_url to the main domain without https
+  # > set gitlab_ssh_host to the main domain
+  # > set gitlab_ssh_port to the custom port (4567)
+  # -> It will auto configure scp-style URL on clone popup
+
+  wex-exec app::config/setValue -b -k=GITLAB_VERSION -v=master-ubuntu
+  wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=grafana
+}
