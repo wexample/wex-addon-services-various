a27cb803a3c2014811d76485357bc112381d4f71
weeger <romain.weeger@gmail.com>
2023-03-03 12:51:37 +0100

    Fix grafana

--- services/grafana/hooks/serviceInstall.sh
+++ services/grafana/hooks/serviceInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-grafanabServiceInstall() {
+grafanaServiceInstall() {
   wex-exec app::config/setValue -b -k=GRAFANA_VERSION -v=9.4.3
   wex-exec app::config/setValue -b -k=MAIN_CONTAINER_NAME -v=grafana
 }
