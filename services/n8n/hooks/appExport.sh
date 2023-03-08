e678a79721c3b43220f2be126284a95e2d8f1422
weeger <romain.weeger@gmail.com>
2023-03-08 09:48:26 +0100

    Improve import / export

--- services/n8n/hooks/appExport.sh
+++ services/n8n/hooks/appExport.sh
@@ -2,6 +2,6 @@
 
 n8nAppExport() {
   # Wrap commands to avoid use permission errors.
-  wex-exec app/exec -c="su -c \"n8n export:workflow --backup --output=/home/node/.n8n/backup/workflow\" node"
-  wex-exec app/exec -c="su -c \"n8n export:credentials --backup --output=/home/node/.n8n/backup/credentials\" node"
+  wex-exec app/exec -c="n8n export:workflow --backup --output=/home/node/.n8n/backup/workflow"
+  wex-exec app/exec -c="n8n export:credentials --backup --output=/home/node/.n8n/backup/credentials"
 }
